528e89d15e8d08ca5bdc06333890feaa8f8695bd
Becky Melvin <rebecca_melvin@progressive.com>
2023-07-25 11:33:35 -0400

    Created counter component w/ redux

---
+++ frontend/app/src/app/features/counter/state/index.ts
@@ -0,0 +1,31 @@
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
+import * as fromCount from './counter.reducer';
+
+export const FEATURE_NAME = 'counterFeature';
+
+export interface CounterState {
+  count: fromCount.CountState;
+}
+
+export const reducers: ActionReducerMap<CounterState> = {
+  count: fromCount.reducer,
+};
+
+// 1. Feature Selector (like querys)
+const selectFeature = createFeatureSelector<CounterState>(FEATURE_NAME);
+
+// 2. A selector per "branch" of the state
+const selectCountBranch = createSelector(selectFeature, (f) => f.count);
+
+// 3. (Optional) you have "helpers"
+
+// 4. What does the component need
+
+export const selectCounterCurrent = createSelector(
+  selectCountBranch,
+  (b) => b.current
+);
---
+++ frontend/app/src/app/state/index.ts
@@ -0,0 +1,5 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+export interface AppState {} // what the application "state" looks like for typescript
+
+export const reducers: ActionReducerMap<AppState> = {}; // handles all activity of that state
